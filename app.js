17fb905e08abf6804dc18c41e481bc7aabee7102
Minsung0301 <chohein3@naver.com>
2022-11-27 20:12:34 +0900

    update

--- app.js
+++ app.js
@@ -1,30 +1,16 @@
-const http =require("http");
-const { url } = require("inspector");
-const app = http.createServer((req, res) =>{
-    res.writeHead(200, { "Content-Type":"text/html; charset=utf-8"});
-    if (req.url === '/'){
-        res.end("여기는 루트입니다")
-    }
-    else if (req.url === '/login'){
-        res.end("여기는 로그인입니다.")
-    }
+"use strict"
+
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+const home = require("./routes/home")
+app.use("/", home)
+
+app.listen(PORT, function (){
+    console.log("서버 가동");
 });
-
-app.listen(3000, ()=>{
-    console.log("http 가동을 통한 서버입니다")
-})
-
-// const express = require("express");
-// const app = express();
-
-// app.get("/", (req, res) => {
-//     res.send("여기는 루트입니다.")
-// });
-
-// app.get("/login", (req, res) => {
-//     res.send("여기는 로그인입니다.")
-// });
-
-// app.listen(3000, function (){
-//     console.log("서버 가동");
-// });.
